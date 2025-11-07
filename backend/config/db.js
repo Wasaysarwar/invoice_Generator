6b3b5143bbb5561412e2ccc416fc6031c15a2f6c
IbadMoin <ibadmoin@gmail.com>
2025-11-08 02:10:45 +0500

    re-sturtured Directory

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+// Load environment variables from the .env file
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://abdulwassay2959_db_user:wasay123%40@cluster0.l9knfnl.mongodb.net/?appName=Cluster0');
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
+    console.log(process.env.MONGO_URI)
     console.error(`❌ MongoDB connection failed: ${error.message}`);
     process.exit(1);
   }
